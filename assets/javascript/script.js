ab1af3b7cf6538c72ff1211ab9716950333abd03
Crystal Espaillat <crystalespaillat@Crystals-MBP.home>
2018-05-25 16:59:02 -0400

    Changed design AGAIN and completed all my if else statements to keep game score. Still need some kind of loop to make the game continue.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,46 +4,73 @@ var alphabet = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"
                 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]                
 
 // Variables are set to track guessed letters, guesses remaining, wins, losses. 
-var guesses = 10;
+var guessedLetters = [];
+var guesses = 5;
 var wins = 0;
 var losses = 0;
-var guessedLetters = [];
 
 // Computer randomly chooses a value from the array above.
-var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-
+var answer = alphabet[Math.floor(Math.random() * alphabet.length)];
 // For testing purposes, we can see the computer's choice in the console.
-console.log(computerChoice);
+console.log(answer);
 
-// When the user presses a key, do the following.... 
+//Display the scores
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("losses").innerHTML = losses;
+document.getElementById("guesses").innerHTML = guesses;
+
+// when the user hits a key ....
 document.onkeypress = function(event) {
-
-    // Store the keys pressed as a variable called userGuess.
     var userGuess = event.key;
 
-        // if the key pressed is the same as the value randomly selected from the array... 
-        if(userGuess === computerChoice) {
-            // Then go up one number on the wins variable.
-            wins++;
-        }
+    // if the key pressed is the same as the value.....
+    if (answer === userGuess) {
 
-        // if the key pressed is NOT the same as the value then take away one guess.
-        else {
-            guesses--;
-            
-            // Stores the userGuess strokes into the guessedLetters array.
-            guessedLetters.push(userGuess);
+        // Go up one number on the wins variable.                    
+        wins++;
 
-            //Displays guessedLetters on page.
-            document.getElementById("guessedLetters").innerHTML = guessedLetters;
-        }
+        //reset the array
+        guessedLetters = [];
 
-        // When there are no remaining guesses, then increment on the losses variable.
-        if(guesses === 0) {
-            losses++;
-        }
+        //reset the guess counter
+        guesses = 5;
 
+        //Display the new number of wins on page.
         document.getElementById("wins").innerHTML = wins;
-        document.getElementById("losses").innerHTML = losses;
+    }
+
+    // if the key pressed is NOT the same as the value.....
+    else {
+
+        // reduce value of remaining guesses by 1.
+        guesses--;
+
+        //Display the new number of guesses on page.
         document.getElementById("guesses").innerHTML = guesses;
+        
+        // Store the guess into the guessedLetters array.
+        guessedLetters.push(userGuess);
+
+        //Displays guessedLetters on page.
+        document.getElementById("guessedLetters").innerHTML = guessedLetters;
+    }
+    
+    
+    // When there are no remaining guesses, then increment on the losses variable.
+    if (guesses === 0) {
+
+        //reset the array
+        guessedLetters = [];
+
+        //reset the guess counter
+        guesses = 5;
+
+        // increments losses
+        losses++;
+
+        // displays the score
+        document.getElementById("losses").innerHTML = losses;
+
+    }
+
 }
