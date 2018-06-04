d7e6d73355f4b90c9260c94630a4bcd4db397078
Crystal Espaillat <crystalespaillat@Crystals-MBP.home>
2018-06-04 06:05:00 -0400

    Finished javascript logic.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,16 +8,22 @@ var guessedLetters = [];
 var guesses = 5;
 var wins = 0;
 var losses = 0;
+var answer;
 
-// Computer randomly chooses a value from the array above.
-var answer = alphabet[Math.floor(Math.random() * alphabet.length)];
-// For testing purposes, we can see the computer's choice in the console.
-console.log(answer);
-
-//Display the scores
+//Display the starting score on the webpage
 document.getElementById("wins").innerHTML = wins;
 document.getElementById("losses").innerHTML = losses;
 document.getElementById("guesses").innerHTML = guesses;
+
+function computerGuess() {
+   // Computer randomly chooses a value from the array above.
+    answer = alphabet[Math.floor(Math.random() * alphabet.length)];
+    // For testing purposes, we can see the computer's choice in the console.
+    console.log(answer);
+    return answer;
+}
+
+computerGuess();
 
 // when the user hits a key ....
 document.onkeypress = function(event) {
@@ -29,14 +35,15 @@ document.onkeypress = function(event) {
         // Go up one number on the wins variable.                    
         wins++;
 
-        //reset the array
-        guessedLetters = [];
-
-        //reset the guess counter
-        guesses = 5;
-
         //Display the new number of wins on page.
         document.getElementById("wins").innerHTML = wins;
+
+        //Display winning message on screen.
+        document.getElementById("message").innerHTML = "<h2>You win! The letter was " + answer + ". <br /> Guess what letter I'm thinking of now.</h2>";
+        
+        // Generate random letter again. 
+        computerGuess();
+
     }
 
     // if the key pressed is NOT the same as the value.....
@@ -53,6 +60,10 @@ document.onkeypress = function(event) {
 
         //Displays guessedLetters on page.
         document.getElementById("guessedLetters").innerHTML = guessedLetters;
+
+         //Display winning message on screen.
+         document.getElementById("message").innerHTML = "<h2>Nope! <br /> Guess Again.</h2>";
+
     }
     
     
@@ -71,6 +82,11 @@ document.onkeypress = function(event) {
         // displays the score
         document.getElementById("losses").innerHTML = losses;
 
+        //Display winning message on screen.
+        document.getElementById("message").innerHTML = "<h2>The letter was " + answer + ". You lose! <br /> Guess what letter I'm thinking of now.</h2>";
+
+        // Generate random letter again
+        computerGuess();
     }
 
 }
